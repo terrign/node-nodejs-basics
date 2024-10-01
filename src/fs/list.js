a25460db5a6e8c06005a7d83c10584da6751c6d8
terrign <vajipi@gmail.com>
2024-10-01 14:30:01 +0300

    fix(fs:list): missing await in if condition

--- src/fs/list.js
+++ src/fs/list.js
@@ -3,7 +3,7 @@ import { exists } from '../utils/index.js';
 import { FILES_DIR, FsError } from './const.js';
 
 const list = async () => {
-  if (exists(FILES_DIR)) {
+  if (await exists(FILES_DIR)) {
     console.log(await fs.readdir(FILES_DIR));
   } else {
     throw new FsError();
