74aa669884183b8625bbdb34d88dde60e1a32021
terrign <vajipi@gmail.com>
2024-10-01 16:22:48 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+
+const SRC_PATH = path.join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  createReadStream(SRC_PATH)
+    .on('data', (chunk) => {
+      hash.update(chunk);
+    })
+    .on('end', () => {
+      console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
