5c70e93c1937fa9e323f9f03dcba408becb2ca44
terrign <vajipi@gmail.com>
2024-10-02 14:23:46 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import { FILES_DIR } from './const.js';
+
+const FILE_PATH = path.join(FILES_DIR, 'fileToCompress.txt');
+
 const compress = async () => {
-    // Write your code here 
+  pipeline(createReadStream(FILE_PATH), createGzip(), createWriteStream(path.join(FILES_DIR, 'fileToCompress.txt.gz')));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import { FILES_DIR } from './const.js';
+
+const FILE_PATH = path.join(FILES_DIR, 'fileToCompress.txt.gz');
+
+const compress = async () => {
+  pipeline(createReadStream(FILE_PATH), createUnzip(), createWriteStream(path.join(FILES_DIR, 'decompressed.txt')));
 };
 
-await decompress();
+await compress();
