2a685a23fe4d667d217eae3a463a4e511ef0ef37
terrign <vajipi@gmail.com>
2024-10-02 21:37:07 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,9 @@
+import { fork } from 'child_process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(path.join(import.meta.dirname, 'files', 'script.js'), args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', '...']);
