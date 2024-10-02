f860024e5debbb1bef9965ed17fdb0fed2d24260
terrign <vajipi@gmail.com>
2024-10-02 20:49:23 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
-const performCalculations = async () => {
-    // Write your code here
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import path from 'path';
+
+const CPUS_COUNT = cpus().length;
+const WORKER_PATH = path.join(import.meta.dirname, './worker.js');
+
+const performCalculations = async (seed = 10) => {
+  const promises = new Array(CPUS_COUNT).fill(null).map(
+    (_, i) =>
+      new Promise((resolve) => {
+        const worker = new Worker(WORKER_PATH, { workerData: { value: seed + i } });
+        worker.on('error', () => resolve({ status: 'error', data: null }));
+        worker.on('message', ({ data }) => resolve({ status: 'resolved', data }));
+      })
+  );
+  Promise.allSettled(promises)
+    .then((it) => it.map(({ value }) => value))
+    .then(console.log);
 };
 
 await performCalculations();
